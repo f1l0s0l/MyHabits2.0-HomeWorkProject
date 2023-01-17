a5b65d78df789b23e63d4dad0ac64e79553bbd8f
Sidorik Ilia <iss19.spb@gmail.com>
2023-01-17 22:33:39 +0700

    Initial Commit

---
+++ MyHabits2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits2.0
+//
+//  Created by Илья Сидорик on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
